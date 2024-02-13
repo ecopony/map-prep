6738920756af4ebd8697471a909834754c9945e5
ecopony <ecopony@gmail.com>
2024-02-12 17:25:03 -0800

    Add finished forest loss

--- utils/hillshade_utils.py
+++ utils/hillshade_utils.py
@@ -4,6 +4,7 @@ from rasterio.merge import merge
 from osgeo import gdal
 import os
 import zipfile
+import numpy as np
 
 def generate_hillshade_raster(directory_path):
     hgt_files = [os.path.join(directory_path, f) for f in os.listdir(directory_path) if f.endswith('.hgt')]
@@ -32,6 +33,26 @@ def generate_hillshade_raster(directory_path):
 
     return out_fp, hillshade_fp
 
+def combine_tif_files(directory_path, output_file):
+    hgt_files = [os.path.join(directory_path, f) for f in os.listdir(directory_path) if f.endswith('.tif')]
+
+    if not hgt_files:
+        raise FileNotFoundError("No TIFF files found in the provided directory.")
+
+    rasters = [rasterio.open(f) for f in hgt_files]
+    combined_raster, out_trans = merge(rasters)
+
+    out_fp = output_file
+    out_meta = rasters[0].meta.copy()
+    out_meta.update({"driver": "GTiff",
+                     "height": combined_raster.shape[1],
+                     "width": combined_raster.shape[2],
+                     "transform": out_trans})
+
+    with rasterio.open(out_fp, "w", **out_meta) as dest:
+        dest.write(combined_raster)
+
+
 def extract_zip_files(directory_path):
     output_directory = os.path.join(directory_path, "output")
     os.makedirs(output_directory, exist_ok=True)
@@ -44,3 +65,9 @@ def extract_zip_files(directory_path):
             zip_ref.extractall(output_directory)
 
     return output_directory
+
+
+
+
+
+
